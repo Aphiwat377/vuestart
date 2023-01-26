8b6e1d949da50e21022b4550dd16d4be49797951
aphiwat <aphiwatchaiya06@gmail.com>
2023-01-26 13:56:49 +0700

    fivelab

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LikeSad from "./components/LikeSad.vue"
+</script>
+
+<template>
+  
+  <div v-for="n in 10" :key="n">{{  }} <LikeSad></LikeSad></div>
+
+</template>
+
+<style scope></style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+
+// Import all of Bootstrap's Iconsimport"bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+createApp(App).mount('#app')
